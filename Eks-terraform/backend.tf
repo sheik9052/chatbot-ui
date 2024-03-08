11ae66c5f1a73f4e6744ef612b0475d9578c1a65
sheik9052 <160618604+sheik9052@users.noreply.github.com>
2024-03-07 21:36:38 -0600

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "awsbucket2290" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
+    key    = "mybuckert.tfstate"
     region = "us-east-1"
   }
 }
